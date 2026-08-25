b38b1c40e7ec9672d15137341c666780aaed31fd
Axel NGuessan <axelsylvain9@gmail.com>
2026-08-25 00:22:11 +0000

    fin de la premiere version final et prete pour la soutenance V2.1

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const ADMIN_ORIGIN = process.env.NODE_ENV === 'production'
-  ? 'https://santeciadmin.vercel.app/'
+  ? 'https://santeciadmin.vercel.app'
   : 'http://localhost:5173';
 
 const nextConfig: NextConfig = {
