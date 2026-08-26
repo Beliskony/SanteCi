57b0a048cba9cc623cc5068cf8591e39ce2201bb
Axel NGuessan <axelsylvain9@gmail.com>
2026-08-26 18:09:43 +0000

    prod v5.1 ajustement de sidebar

--- app/frontend/components/dashboard/patient/PatSideBar.tsx
+++ app/frontend/components/dashboard/patient/PatSideBar.tsx
@@ -91,8 +91,8 @@ const SidebarContent = ({
       {/* Logo */}
       <div className="flex items-center gap-3 px-2 mb-8">
         <Link href="/" className="flex items-center gap-3 px-2 mb-8">
-        <div className="flex items-center gap-3 shrink-0">
-          <div className="w-20 h-20 rounded-xl">
+        <div className="flex items-center justify-center gap-3 shrink-0">
+          <div className="w-30 h-30 rounded-xl">
             <img src={'/icon/favicon.svg'} className="w-full h-full object-cover" />
           </div>
         </div>
